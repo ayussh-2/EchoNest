d45bca8f483ac62e63646416174c25cc29226de1
ayussh-2 <ayushatschool5@gmail.com>
2024-03-11 21:42:25 +0530

    segregated player

--- src/components/MainPlayer.jsx
+++ src/components/MainPlayer.jsx
@@ -1,9 +1,12 @@
 import PlayerHeader from "./PlayerHeader";
-
+import AlbumCover from "./AlbumCover";
+import MusicDetails from "./MusicDetails";
 function MainPlayer() {
     return (
-        <div>
+        <div className="py-10 px-5 font-poppins">
             <PlayerHeader />
+            <AlbumCover />
+            <MusicDetails />
         </div>
     );
 }
